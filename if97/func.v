9494b05b23279c1459ce1fde572b9684fed3aceb
amoyyy <yuyangsansi@163.com>
2020-06-18 15:58:14 +0800

    modified:   if97/func.v

--- if97/func.v
+++ if97/func.v
@@ -16,7 +16,6 @@
  *
  **********************************************************************/
  module if97
-
 /* Number Functions*/
 /* square */
 fn sq(x f64) f64 {
@@ -29,7 +28,7 @@ fn cube(x f64) f64 {
 }
 
 /* fabs */ 
-pub fn fabs(x f64) f64 {
+fn fabs(x f64) f64 {
 	//*(((int *) &x) + 1) &= 0x7fffffff;
 	if x < 0 {return -x}
 	return x
@@ -98,77 +97,3 @@ fn ipow2(x1_in f64, x2_in f64, n1_in int, n2_in int) f64{
 
 	return t
 }
-
-/*
-Rejected since issue #5082: cannot define methods on types from other modules
-/* Polynomial ops  YU YANG*/
-fn (coeff []f64) po_mul(vars f64) f64 {
-	mut sum := 0.0
-	for i:=0; i < coeff.len - 1; i++{
-		sum += coeff[i]
-		sum *= vars
-	}
-	sum += coeff[coeff.len-1]
-	return sum
-}
-
-fn (coeff []f64) po_mul_reverse(vars f64) f64 {
-	mut sum := 0.0
-	for i:=coeff.len - 1; i >= 0; i--{
-		sum *= vars
-		sum += coeff[i]
-	}
-	return sum
-}
-
-/* Array ops  YU YANG*/
-fn (mut a []f64) add(input []f64){
-	//assert a.len == input.len
-	for i:=0; i < a.len; i++{
-		a[i] += input[i]
-	}
-}
-
-fn (mut a []f64) sub(input []f64){
-	assert a.len == input.len
-	for i:=0; i < a.len; i++{
-		a[i] -= input[i]
-	}
-}
-
-fn (mut a []f64) mul(input f64){
-	for i:=0; i < a.len; i++{
-		a[i] *= input
-	}
-}
-
-
-
-
-fn r1_cal_pow(p f64, T f64) ([]f64, []f64) {
-	pi, tau := r1_pitau(p, T)
-	pii := 7.1 - pi 
-	mut ipow_ := []f64{len: 33}
-	mut jpow_ := []f64{len: 42}
-	ipow_[0] = 1.0
-	ipow_[1] = pii
-	ipow_[2] = sq(pii)
-	ipow_[3] = ipow_[1]*ipow_[2]
-	ipow_[4] = ipow_[2]*ipow_[2]
-	ipow_[5] = ipow_[2]*ipow_[3]
-	ipow_[8] = ipow_[4]*ipow_[4]
-	ipow_[21] = ipow_[8]*ipow_[8]*ipow_[5]
-	ipow_[23] = ipow_[2]*ipow_[21]
-	ipow_[29] = ipow_[23]*ipow_[5]*pii
-	ipow_[30] = ipow_[29]*pii
-	ipow_[31] = ipow_[30]*pii
-	ipow_[32] = ipow_[31]*pii
-
-	mut jmul := 1.0
-	for j:=0;j<42;j++{
-		jpow_[j] = jmul 
-		jmul *= tau - 1.222
-	}
-	return ipow_, jpow_
-}
-*/
